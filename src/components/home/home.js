b0b478d1b82cecc64ed01f486e4ed7138687c3ef
sepideh maghami <sepidehmaghamiit@gmail.com>
2023-01-17 22:46:33 +0330

    first commit

---
+++ src/components/home/home.js
@@ -0,0 +1,13 @@
+import './home.css';
+
+function Home() {
+  return (
+    <div className="home">
+        <h1>
+          Beauty navigations
+        </h1>
+    </div>
+  );
+}
+
+export default Home;
