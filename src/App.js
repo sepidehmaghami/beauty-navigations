4c789fd9a57169c15c0f6c1b0818c735af69c773
sepideh maghami <sepidehmaghamiit@gmail.com>
2023-01-19 23:03:54 +0330

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import Home from "./components/home/home";
+import {Route ,Routes,Navigate } from 'react-router-dom'
+import Nav1 from './components/nav1/nav1';
+import Nav2 from './components/nav2/nav2';
+import Nav3 from './components/nav3/nav3';
+
 function App() {
   return (
     <div>
-        <Home/>
+      
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/nav1" element={<Nav1/>}/>
+          <Route path="/nav2" element={<Nav2/>}/>
+          <Route path="/nav3" element={<Nav3/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,9 @@
 import './home.css';
+import { Link } from 'react-router-dom';
+import Nav1 from '../asserts/nav1.jpg'
+import Nav2 from '../asserts/nav2.jpg';
+import Nav3 from '../asserts/nav3.jpg';
+import Nav4 from '../asserts/nav4.jpg';
 
 function Home() {
   return (
@@ -6,6 +11,38 @@ function Home() {
         <h1>
           Beauty navigations
         </h1>
+        <div className='navs'>
+          <div className='home-cart'>
+            <Link to='/nav1' className='toDoList'>
+              <img src={Nav1} alt="ToDoList"/>
+              <p>navigation 1</p>
+            </Link>
+          </div>
+
+          <div className='home-cart'>
+            <Link to='/nav2' className='toDoList'>
+              <img src={Nav2} alt="ToDoList"/>
+              <p>navigation 2</p>
+            </Link>
+          </div>
+
+          </div>
+
+        <div className='navs'>
+          <div className='home-cart'>
+            <Link to='/nav3' className='toDoList'>
+              <img src={Nav3} alt="ToDoList"/>
+              <p>navigation 3</p>
+            </Link>
+          </div>
+
+          <div className='home-cart'>
+            <Link to='/nav4' className='toDoList'>
+              <img src={Nav4} alt="ToDoList"/>
+              <p>navigation 4</p>
+            </Link>
+          </div>
+        </div>
     </div>
   );
 }
