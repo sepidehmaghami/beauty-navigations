749fd9ddad61ec4d8f0cc16836398b5f6e9e5b54
sepideh maghami <sepidehmaghamiit@gmail.com>
2023-05-19 22:26:16 +0430

    completed navigation 2

--- src/components/nav2/nav2.js
+++ src/components/nav2/nav2.js
@@ -1,64 +1,29 @@
 import './nav2.css';
-import { useState } from 'react';
 import BackBtn from '../backBtn/backBtn';
-import { AiTwotoneHome, AiOutlineTwitter, AiFillBell , AiOutlineUser} from "react-icons/ai"
+import { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 
 function Nav2() {
-  const [check , setCheck]= useState([
-    {
-      flag: true
-    },
-    {
-      flag: false
-    },
-    {
-      flag: false
-    },
-    {
-      flag: false
-    }
-  ]);
-  const selectNav =(e) =>{
-    if(!check){
-        setCheck(true);
-    }       
-    else{
-        setCheck(false);          
-    }
-  }
+  const [value, setValue] = useState(0);
 
-  const extra_styles = (check) => {
-    if (check) {
-        return {
-            color: "#2ee197",
-            fontSize: "35PX"
-        };
-        }
-    else {
-        return {
-          color: "rgb(103, 103, 103)",
-          fontSize: "30PX"
-        }
-    }
-};
-  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="navigation-two">
-        <ul className='nav-two'>
-          <li>
-            <AiTwotoneHome style={extra_styles(check[0].flag)} onClick={e => selectNav(0)} />
-          </li>
-          <li>
-            <AiOutlineTwitter style={extra_styles(check[1].flag)} onClick={e => selectNav(1)} />
-          </li>
-          <li>
-            <AiFillBell style={extra_styles(check[2].flag)} onClick={e => selectNav(2)}/>
-          </li>
-          <li>
-            <AiOutlineUser style={extra_styles(check[3].flag)} onClick={e => selectNav(3)}/>  
-          </li>
-        </ul>
-        <BackBtn/>
+    <div >
+      <AppBar position="static" className="navigation-two">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Item One"  />
+          <Tab label="Item Two"  />
+          <Tab label="Item Three"  />
+        </Tabs>
+      </AppBar>
+  
+      <BackBtn/>
     </div>
   );
 }
